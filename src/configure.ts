88dfd0554e06d0de2c37e674f171692227e5a7fd
terukazu inoue <terukazu.inoue@codianz.com>
2023-07-03 19:19:19 +0900

    parameter error implementation and error handling test code

--- src/configure.ts
+++ src/configure.ts
@@ -1,4 +1,3 @@
-
 export class Configure<WINS extends string, LOSES extends string>
 {
   wins: readonly WINS[];
@@ -7,6 +6,14 @@ export class Configure<WINS extends string, LOSES extends string>
   gain: number;
 
   constructor(wins: readonly WINS[], loses: readonly LOSES[], ratio: { [_ in WINS | LOSES]: number; }, gain: number) {
+    if(loses.length == 0){
+      throw new Error("specify at least one `loses` element.");
+    }
+    [...wins, ...loses].forEach((key) => {
+      if(ratio[key] < 1){
+        throw new Error("the numerical value of each element of `ratio` is 1 or more.");
+      }
+    });
     this.wins = wins;
     this.loses = loses;
     this.ratio = ratio;
